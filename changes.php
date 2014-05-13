27a434c507615f90dda6443cbf9ad9cc40d69e11
Jānis Rūcis <parasti@gmail.com>
2014-05-14 02:07:13 +0300

    Factor out cache file generation

--- changes.php
+++ changes.php
@@ -1,4 +1,6 @@
 <?php
+require("includes/cache.php");
+
 $title = "Neverball - Changes";
 include("includes/header.php");
 ?>
@@ -11,59 +13,16 @@ include("includes/header.php");
 
 	<div id="main">
 		<div id="contents">
-		<h2>Changes</h2><?php
-function parse_changes($fname){
-	$cnf = file($fname);
-	$ret = "\n";
-	$state = 0;
-	
-	foreach($cnf as $line){
-		if(preg_match('/^-+$/', $line)){
-			if($state == 2){
-				$ret .= "</li>\n\t\t";
-			}
-			if($state == 1 || $state == 2){
-				$ret .= "</ul>\n";
-			}
-			$state = 0;
-		}else if(preg_match('/^(New in .+)$/', $line, $matches) && ($state == 0)){
-			$ret .= "\t\t<h3>".htmlentities($matches[1])."</h3>\n\t\t";
-			$ret .= "<ul>\n\t\t";
-			$state = 1;
-		}else if(preg_match('/^\* ([^ ].+)$/', $line, $matches) && ($state == 1 || $state == 2)){
-			if($state == 2){
-				$ret .= "</li>\n\t\t";
-			}
-			$ret .= "\t<li>".htmlentities($matches[1]);
-			$state = 2;
-		}else if(preg_match('/^  ([^ ].+)$/', $line, $matches) && ($state == 2)){
-			$ret .= " ".htmlentities($matches[1]);
-			$state = 2;
-		}
-	}
-	if($state == 2){
-		$ret .= "</li>\n\t\t";
-	}
-	if($state == 1 || $state == 2){
-		$ret .= "</ul>\n\t\t";
-	}
-	return $ret;
-}
-if((file_exists("cache/changelog.php") && !file_exists("CHANGES")) || (file_exists("cache/changelog.php") && (filemtime("CHANGES") == filemtime("cache/changelog.php")))){
-	include("cache/changelog.php");
-}else if(!file_exists("cache/changelog.php") && !file_exists("CHANGES")){
+		<h2>Changes</h2>
+<?php
+$cachefile = cache_file("CHANGES", "cache/changelog.php");
+
+if ($cachefile !== false)
+	include($cachefile);
+else
 	echo "\n\t\t<p>Failed to read CHANGES data!</p>\n";
-}else{
-	$changesFile = parse_changes("CHANGES");
-	$changesFileWrite = "<?php\nif(basename(\$_SERVER[\"SCRIPT_NAME\"]) == \"changelog.php\"){\n\tdie();\n}\n?>\n".$changesFile;
-	$fh = fopen('cache/changelog.php', 'w') or die("Failed to open changelog.php for writing!");
-	fwrite($fh, $changesFileWrite);
-	fclose($fh);
-	chmod("cache/changelog.php", 0666);
-	touch("cache/changelog.php", filemtime("CHANGES"));
-	echo $changesFile;
-}
-?></div>
+?>
+</div>
 <?php
 include("includes/footer.php");
 ?>
--- contributors.php
+++ contributors.php
@@ -1,4 +1,6 @@
 <?php
+require("includes/cache.php");
+
 $title = "Neverball - Contributors";
 include("includes/header.php");
 ?>
@@ -12,54 +14,15 @@ include("includes/header.php");
 	<div id="main">
 		<div id="contents">
 		<h2>Neverball Contributions</h2>
-		<dl class="add-colons"><?php
-		if((file_exists("cache/authors.php") && !file_exists("AUTHORS")) || (file_exists("cache/authors.php") && file_exists("AUTHORS") && (filemtime("AUTHORS") == filemtime("cache/authors.php")))){
-			include("cache/authors.php");
-		}else if(!file_exists("cache/authors.php") && !file_exists("AUTHORS")){
-			echo "\n\t\t<dd>Failed to read AUTHORS data!</dd>\n";
-		}else{
-			function file_get_contents_utf8($fn){
-			        $content = file_get_contents($fn);
-			        return mb_convert_encoding($content, 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
-			}
-			function regex_add_parens($addTo){
-			        $addTo[0] = preg_replace("/\nA:\s/", " (", $addTo[0]);
-					$addTo[0] = preg_replace("/\n/", "", $addTo[0]);
-			        return $addTo[0].")\n";
-			}
-			function regex_add_close_dt($addTo){
-			        $addTo[0] = preg_replace("/\n/", "", $addTo[0]);
-			        return $addTo[0]."</dt>\n";
-			}
-			function regex_add_close_li($addTo){
-			        $addTo[0] =  preg_replace("/\n/", "", $addTo[0]);
-			        return $addTo[0]."</li>\n";
-			}
-			function regex_add_parens_and_format($addTo){
-			        $addTo[0] = preg_replace("/A:\s/", "\t\t\t</ul></dd>\n\t\t\t<dt>", $addTo[0]);
-					$addTo[0] = preg_replace("/\n/", "", $addTo[0]);
-			        return $addTo[0]."</dt>\n\t\t\t<dd><ul>\n";
-			}
-			$authorsFile = file_get_contents_utf8("AUTHORS");
-			$authorsFile = preg_replace("/(#.*\n|E:.*\n|P:.*\n)/", "", $authorsFile); //Remove comments, emails and PGP keys
-			$authorsFile = preg_replace("/\n*\n/", "\n", $authorsFile); //Remove un-necessary newlines
-			$authorsFile = preg_replace("/N:\s/", "\t\t\t<dt>", $authorsFile); //Add <dt> tags to beginning of names
-			$authorsFile = preg_replace_callback("/<dt>.*\nA:\s.*\n/", "regex_add_parens", $authorsFile); //Add () around forum handles
-			$authorsFile = preg_replace_callback("/<dt>.*\n/", "regex_add_close_dt", $authorsFile); //Add ending </dt> tag to end of names
-			$authorsFile = preg_replace("/(W: |D: )/", "\t\t\t\t<li>", $authorsFile); //Add <li> tags to beginning of websites and descriptions
-			$authorsFile = preg_replace_callback("/<li>.*\n/", "regex_add_close_li", $authorsFile); //Add <li> tags to end of emails, websites and descriptions
-			$authorsFile = preg_replace("/<\/dt>\n\t\t\t\t<li>/", "</dt>\n\t\t\t<dd><ul>\n\t\t\t\t<li>", $authorsFile); //Insert <dd> and <ul> tags
-			$authorsFile = preg_replace("/<\/li>\n\t\t\t<dt>/", "</li>\n\t\t\t</ul></dd>\n\t\t\t<dt>", $authorsFile); //Insert </ul> and </dd> tags
-			$authorsFile = preg_replace_callback("/A:\s.*\n/", "regex_add_parens_and_format", $authorsFile); //Format authors forum names without proper real names
-			$authorsFile = $authorsFile."\n\t\t\t</ul></dd>\n";
-			$authorsFileWrite = "<?php\nif(basename(\$_SERVER[\"SCRIPT_NAME\"]) == \"authors.php\"){\n\tdie();\n}\n?>\n".$authorsFile;
-			$fh = fopen('cache/authors.php', 'w') or die("Failed to open authors.php for writing!");
-			fwrite($fh, $authorsFileWrite);
-			fclose($fh);
-			chmod("cache/authors.php", 0666);
-			touch("cache/authors.php", filemtime("AUTHORS"));
-			echo $authorsFile;
-		}?>
+		<dl class="add-colons">
+<?php
+$cachefile = cache_file("AUTHORS", "cache/authors.php");
+
+if ($cachefile !== false)
+	include($cachefile);
+else
+	echo "\n\t\t<dd>Failed to read author data!</dd>\n";
+?>
 		</dl>
 		<br>
 		<h2>Website Contributions</h2>
