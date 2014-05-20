456f450035b6b11f4972137709204519921822d7
Jānis Rūcis <parasti@gmail.com>
2014-05-20 16:00:37 +0300

    Update home page for 1.6.0

--- index.php
+++ index.php
@@ -11,17 +11,22 @@ include("includes/header.php");
 
 	<div id="main">
 		<div id="contents">
+
+		<h2>News</h2>
+		<h3>2014-05-20: Neverball 1.6.0</h3>
+		<p>Neverball 1.6.0 is out now! Get it from the <a href="download.php">download page</a> and have a look at the <a href="release.php">release notes</a>.</p><p>Long-time contributor Josh Bush has put together a <a href="images/neverball_poster_1.6.0.png">poster</a> and an <a href="https://www.youtube.com/watch?v=ZQ9VDabHGpA">introductory video</a> to celebrate the release. Be sure to check them out!</p>
+
 		<h2>About</h2>
 		<p>Tilt the floor to roll a ball through an obstacle course before time runs out. Neverball is part puzzle game, part action game, and entirely a test of skill.</p>
 		<p>Also found here is Neverputt, a hot-seat multiplayer miniature golf game, built on the physics and graphics engine of Neverball.</p>
-		<p>Neverball and Neverputt are both known to run under Linux, Windows, FreeBSD, and Mac OSX. Hardware accelerated OpenGL is required. A 500MHz processor is recommended.</p>
-		<h2>Status</h2>
-		<p>The current version includes 141 Neverball levels and 134 Neverputt holes.</p>
+		<p>Neverball is known to run under Linux, Windows, FreeBSD, and Mac OSX. Hardware accelerated OpenGL is required. A modern processor is recommended.</p>
+
 		<h2>Community &amp; Contact</h2>
-		<p>Neverball fans shino and mym have put together a <a href="http://www.nevercorner.net/table/" onClick="javascript: pageTracker._trackPageview('/outgoing/nevertable');">Neverball Hall of Fame</a>. This site catalogs hundreds of crazy runs and high scores made by expert players. Take a look and witness some very impressive Neverball play. If you've got interesting replays of your own, be sure to submit them!</p>
-		<p>Visit the <a href="http://www.nevercorner.net/forum/" onClick="javascript: pageTracker._trackPageview('/outgoing/neverforum');">Discussion Forum</a> to get in touch with the developers, or if you have questions, want to report bugs, or simply if you would like to chat with other players.</p>
-		<h2>Mapping</h2>
-		<p>Players interested in trying their hand at level creation are encouraged to install <a href="http://ingar.satgnu.net/gtkradiant/">NetRadiant</a> and read through the <a href="http://wiki.nevercorner.net/doku.php?id=level_creation" onClick="javascript: pageTracker._trackPageview('/outgoing/neverwiki');">level design documentation</a>. Levels of sufficent quality may be included in the next version; authors will be credited in-game.</p>
+		<p>Visit the <a href="http://forum.nevercorner.net/" onClick="javascript: pageTracker._trackPageview('/outgoing/neverforum');">Discussion Forum</a> to get in touch with other players and contributors.</p>
+		<p>Neverball fans shino and mym have put together a <a href="http://table.nevercorner.net/" onClick="javascript: pageTracker._trackPageview('/outgoing/nevertable');">Neverball Hall of Fame</a>. This site catalogs hundreds of crazy runs and high scores made by expert players. Take a look and witness some very impressive Neverball play. If you've got interesting replays of your own, be sure to send them in!</p>
+
+		<h2>Level Design</h2>
+		<p>Players interested in trying their hand at level creation are encouraged to read through the <a href="http://github.com/neverball/neverball-docs" onClick="javascript: pageTracker._trackPageview('/outgoing/documentation');">level design documentation</a>. Levels of sufficent quality may be included in the next version; authors will be credited in-game.</p>
 		</div>
 <?php
 include("includes/footer.php");
