4ed2be7ed66297fae06b35d2d1f2417a56069f32
ErsohnStyneEntertainment <ersohn.styne@gmail.com>
2022-12-08 14:38:09 +0100

    Created community standards from Microsoft

---
+++ community-standards.php
@@ -0,0 +1,40 @@
+<?php
+$title = "Neverball - Community Standards";
+include("includes/header.php");
+?>
+</head>
+<body>
+	<div id="wrapper">
+	<div id="banner"><h1>Neverball</h1></div>
+
+<?php include("includes/nav.php"); ?>
+
+    <div id="main">
+        <div id="contents">
+            <h2>Pennyball + Neverball community standards</h2>
+            <p>Your use of Pennyball and Neverball is governed by the <a href="https://microsoft.com/en-us/servicesagreement">Microsoft Services Agreement</a> ("MSA"). While the <a href="https://microsoft.com/servicesagreement">Code of conduct</a> section of the <a href="https://microsoft.com/servicesagreement">Microsoft Services Agreement</a> applies to all Microsoft products as well as the <a href="https://www.xbox.com/en-US/legal/community-standards?xr=footnav">Xbox Community Standards</a>, as the Pennyball development continues to grow and introduce new features including new ways to interact with others that benefit from an additional level of explanation. To this end, we've created the following Community Standards for the Pennyball and Neverball. Consider these standards a guide for how the Pennyball community should interact with each other and the game to keep our community a positive, safe, and enjoyable place for everyone.</p>
+            <p>We want to ensure players have the same, inclusive and welcoming game experience when playing any of our games. We also want to ensure that if players choose to not follow the Community Standards in one of the Neverball development, we can protect players across the franchise and in our community.</p>
+            <p>These handy Community Standards cover gameplay using GitHub-provided online services. On top of these, individual titles and sites may have additional policies specific to them - so make sure to check them out, too.</p>
+
+            <ul>
+                <li>Treat other community members with respect. This is core to everything we believe in. When you express your opinions, please do so politely and respectfully.</li>
+                <li>If there's a chat function available in a game or on a website, be mindful of other people's experience. This includes avoiding excessive posting or spamming. Also, all-caps is often read as YELLING! And yelling at someone is usually not very nice.</li>
+                <li>If you have an argument or unpleasant encounter with another player, our lovely moderation team are available to advise and assist. Please contact them and/or report the player using the tools available within the game. Whatever the situation, don't "name and shame" other members of the community publicly as this counts as harassment.</li>
+                <li>Think twice about the information that you share publicly. Do not post personal information (phone numbers, email addresses, passwords, account information, physical addresses, first and last names, etc) about yourself or others.</li>
+                <li>You are not allowed to promote any manner of illegal activity.</li>
+                <li>Create content and conversation that is positive and encouraging, rather than negative and disparaging. It should go without saying, but don't ever create posts or content with the purpose of starting a fight or to elicit a negative response.</li>
+                <li>Pennyball has a zero-tolerance policy towards hate speech, bullying, harassing, sexual solicitation, or threatening others.</li>
+                <li>Do not share your account information with anyone. We will never ask for your password, and you shouldn't give it.</li>
+            </ul>
+
+            <h2>How to submit a case review</h2>
+            <p>Any player that meets the following criteria may submit a case review:</p>
+            <ul>
+                <li>Has an active enforcement action.</li>
+                <li>The enforcement's duration is greater than 24 hours in length.</li>
+                <li>The enforcement was issued in the last 12 months.</li>
+            </ul>
+            <p>If your enforcement action meets the criteria, here's how you submit a <a href="https://forms.gle/JRpacoPkBfW5zPY58">Case Review</p>.</p>
+        </div>
+
+<?php include("includes/footer.php"); ?>
