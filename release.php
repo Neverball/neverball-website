ef72b734269b098757b4d5e03ab174df638eebbf
Jānis Rūcis <parasti@gmail.com>
2024-06-23 15:15:58 +0300

    Fix release template

--- release.php
+++ release.php
@@ -4,25 +4,18 @@ require("includes/cache.php");
 $title = "Neverball - Release Notes";
 include("includes/header.php");
 ?>
-</head>
-<body>
-	<?php include("includes/nav.php"); ?>
+<div id="main">
+	<div id="contents">
+		<?php
+		$cachefile = cache_file("docs/release-notes.md", "cache/release.php");
 
-	<h1 id="banner" class="neverball-text">Neverball</h1>
-
-	<div id="main">
-		<div id="contents">
-			<?php
-			$cachefile = cache_file("docs/release-notes.md", "cache/release.php");
-
-			if ($cachefile !== false)
-				include($cachefile);
-			else
-				echo '<p>Failed to read release notes!</p>';
-			?>
-		</div>
+		if ($cachefile !== false)
+			include($cachefile);
+		else
+			echo '<p>Failed to read release notes!</p>';
+		?>
 	</div>
+</div>
 
-	<?php
-	include("includes/footer.php");
-	?>
+<?php
+include("includes/footer.php");
