78584a3df20ebcc8b1ca23fb9e3f0f9e7f4a611d
Jānis Rūcis <parasti@gmail.com>
2026-09-08 12:05:45 +0300

    Add meta tags and stylesheet cache-busting

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,10 @@ global $current_route;
 
 $canonical_query = http_build_query(array_intersect_key($_GET, array_flip(['id', 'set'])));
 $canonical_link = BASE_URL . ($current_route ? '/' . $current_route : '') . ($canonical_query ? '?' . $canonical_query : '');
+
+$meta_description = $description ?? "Neverball is a free, open-source 3D skill and puzzle game. Tilt the floor to roll through obstacle courses. Play in your browser or download for desktop.";
+$meta_title = $title ?? "Neverball";
+$meta_image = BASE_URL . "/images/anim.jpg";
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +14,21 @@ $canonical_link = BASE_URL . ($current_route ? '/' . $current_route : '') . ($ca
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo htmlspecialchars($meta_description); ?>">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="<?php echo $canonical_link; ?>">
+	<meta property="og:title" content="<?php echo htmlspecialchars($meta_title); ?>">
+	<meta property="og:description" content="<?php echo htmlspecialchars($meta_description); ?>">
+	<meta property="og:image" content="<?php echo $meta_image; ?>">
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:url" content="<?php echo $canonical_link; ?>">
+	<meta name="twitter:title" content="<?php echo htmlspecialchars($meta_title); ?>">
+	<meta name="twitter:description" content="<?php echo htmlspecialchars($meta_description); ?>">
+	<meta name="twitter:image" content="<?php echo $meta_image; ?>">
 
 	<link rel="icon" href="/images/favicon-modern.svg">
 	<link rel="canonical" href="<?php echo $canonical_link; ?>">
@@ -17,7 +36,7 @@ $canonical_link = BASE_URL . ($current_route ? '/' . $current_route : '') . ($ca
 	<title><?php echo $title; ?></title>
 
 	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="/css/neverstyle/default.css?v=v" type="text/css" title="Neverstyle">
+	<link rel="stylesheet" href="/css/neverstyle/default.css?v=<?php echo filemtime(BASE_DIR . '/css/neverstyle/default.css'); ?>" type="text/css" title="Neverstyle">
 </head>
 <body>
 	<input type="checkbox" id="menu-toggle" class="hidden" autocomplete="off">
